c4cf0cb84d2fb1aadfa21b282cee927e6224d558
codehunter <smagucha@gmail.com>
2021-01-23 13:44:10 +0300

    add procfile

--- magucha/settings.py
+++ magucha/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -122,3 +122,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIR=(
     os.path.join(BASE_DIR,'static')
     )
+django_heroku.settings(locals())

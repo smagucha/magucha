b2b0d030625d1088ffcebcff5e11009c99cce858
codehunter <smagucha@gmail.com>
2021-01-23 16:00:59 +0300

    staticfiles

--- magucha/settings.py
+++ magucha/settings.py
@@ -119,7 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR=(
-    os.path.join(BASE_DIR,'static')
-    )
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 django_heroku.settings(locals())
